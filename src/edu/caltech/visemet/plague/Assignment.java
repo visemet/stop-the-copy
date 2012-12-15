ec6e8df6de3ae00145708a22b54642d4391a599a
Max Hirschhorn <maxh@caltech.edu>
2012-12-14 16:38:04 -0800

    Made Assignment implement Iterable<List<Submission>>.
    Uses the CombinationIterator class to iterate through all pairs of
    submissions.

--- src/edu/caltech/visemet/plague/Assignment.java
+++ src/edu/caltech/visemet/plague/Assignment.java
@@ -1,6 +1,5 @@
 package edu.caltech.visemet.plague;
 
-import com.google.common.collect.ImmutableList;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
@@ -11,7 +10,9 @@ import java.util.List;
  *
  * @author Max Hirschhorn #visemet
  */
-public class Assignment {
+public class Assignment implements Iterable<List<Submission>> {
+
+    private static final int PAIR = 2;
 
     private final String filename;
 
@@ -27,8 +28,13 @@ public class Assignment {
         submissions.add(submission);
     }
 
-    public Iterator<SubmissionPair> iterator() {
-        return new AssignmentIterator(submissions);
+    @Override
+    public Iterator<List<Submission>> iterator() {
+        return iterator(PAIR);
+    }
+
+    public Iterator<List<Submission>> iterator(int numSubmissions) {
+        return new CombinationIterator<>(numSubmissions, submissions);
     }
 
     public FilenameFilter asFilter() {
@@ -37,57 +43,6 @@ public class Assignment {
         }
 
         return filter;
-    }
-
-    public class SubmissionPair {
-
-        private final Submission submission1;
-
-        private final Submission submission2;
-
-        public SubmissionPair(Submission submission1, Submission submission2) {
-            this.submission1 = submission1;
-            this.submission2 = submission2;
-        }
-    }
-
-    private class AssignmentIterator implements Iterator<SubmissionPair> {
-
-        private final List<Submission> submissions;
-
-        private int index1 = 0;
-
-        private int index2 = 1;
-
-        public AssignmentIterator(List<Submission> submissions) {
-            this.submissions = ImmutableList.copyOf(submissions);
-        }
-
-        @Override
-        public boolean hasNext() {
-            return index2 < submissions.size();
-        }
-
-        @Override
-        public SubmissionPair next() {
-            Submission submission1 = submissions.get(index1);
-            Submission submission2 = submissions.get(index2);
-
-            index2++;
-
-            if (index2 == submissions.size()) {
-                index1++;
-                index2 = index1 + 1;
-            }
-
-            return new SubmissionPair(submission1, submission2);
-        }
-
-        @Override
-        public void remove() {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-
     }
 
     private class AssignmentFilter implements FilenameFilter {
