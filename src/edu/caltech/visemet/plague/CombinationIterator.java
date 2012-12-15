24668d7749ed8679e1f5f2ea5133336c3c2115d9
Max Hirschhorn <maxh@caltech.edu>
2012-12-14 16:34:50 -0800

    Created CombinationIterator class.
    Iterates through combinations of the specified list of choices using the
    indicated subset size.

---
+++ src/edu/caltech/visemet/plague/CombinationIterator.java
@@ -0,0 +1,65 @@
+package edu.caltech.visemet.plague;
+
+import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ *
+ * @author Max Hirschhorn #visemet
+ */
+public class CombinationIterator<T> implements Iterator<List<T>> {
+
+    private final int[] indexes;
+
+    private final List<T> choices;
+
+    public CombinationIterator(int k, List<T> choices) {
+        indexes = new int[k];
+        for (int index = 0; index < k; index++) {
+            indexes[index] = index;
+        }
+
+        this.choices = ImmutableList.copyOf(choices);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return indexes[indexes.length - 1] < choices.size();
+    }
+
+    @Override
+    public List<T> next() {
+        List<T> tuple = new ArrayList<>();
+
+        for (int index : indexes) {
+            tuple.add(choices.get(index));
+        }
+
+        int index = indexes.length - 1;
+
+        indexes[index]++;
+
+        while (indexes[index] == choices.size() - (indexes.length - index - 1)) {
+            if (index == 0) {
+                break;
+            }
+
+            index--;
+            indexes[index]++;
+        }
+
+        index++;
+        for (; index < indexes.length; index++) {
+            indexes[index] = indexes[index - 1] + 1;
+        }
+
+        return tuple;
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
